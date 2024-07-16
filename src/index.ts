7c598a4041db00cf8e7dedd886fd4845bc279177
= <=>
2024-07-15 21:32:00 -0300

    Add validations to field

--- src/index.ts
+++ src/index.ts
@@ -15,6 +15,17 @@ export default definePanel({
 			meta: {
 				interface: 'input',
 				width: 'full',
+				required: true,
+				validation: {
+					_and: [
+						{
+							url: {
+								_regex: "https?:\/\/(www\.)?[-a-zA-Z0-9@:%._\+~#=]{2,256}\.[a-z]{2,4}\b([-a-zA-Z0-9@:%_\+.~#?&//=]*)"
+							}
+						}
+					]
+				},
+				validation_message: "The value should be a valid url"
 			},
 		},
 	],
